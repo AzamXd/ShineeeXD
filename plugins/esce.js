ef47a476030a33cf32c16e4d9ebe5d7f844faed2
AzamXd <108068562+AzamXd@users.noreply.github.com>
2022-10-06 21:36:04 +0700

    Update esce.js

--- plugins/esce.js
+++ plugins/esce.js
@@ -4,7 +4,7 @@
 let handler = async (m, { conn, command }) => {
     conn.reply(m.chat, `*BETA V2.0.1.6*
 • *Multi Auth ( Multiple File Session )*
-• https://github.com/BOTCAHX/RT-X-MD
+• https://telegra.ph/tumbnail-10-06
 • Script ini mempunyai multiple session 
 • *Jika ingin melihat apa itu multiple session lihat link di bawah ini.*
 • *Preview* : https://telegra.ph/file/fd3362fea5132cc59fed2.jpg 
