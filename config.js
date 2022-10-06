f754a75e154aad5fff2f5bf0188de3f9982d5613
AzamXd <108068562+AzamXd@users.noreply.github.com>
2022-10-06 21:30:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6282221792667', 'TioXd', true]
+  ['6281389368689', 'ShineeXD', true]
 // [owner bukan bot!]
 ] // paste nomor di sini
-global.mods = ['6282221792667'] // bantuan?
-global.prems = ['6282221792667'] // user premium limit tanpa batas 
+global.mods = ['6281389368689'] // bantuan?
+global.prems = ['6281389368689'] // user premium limit tanpa batas 
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrttm: 'https://nurutomo.herokuapp.com',
@@ -20,9 +20,9 @@ global.APIKeys = { // APIKey Here
 }
 
 // Wm untuk stiker dan autobio
-global.tio = 'TioXd'
-global.packname = '© BOTCAHX'
-global.author = 'TioXd'
+global.tio = 'ShineeXD'
+global.packname = 'ShineeXD'
+global.author = 'zam'
 
 global.multiplier = 70 // Semakin Tinggi levelmu semakin naik
 
